cffae3baadd692fb2013e6329e613eaacf7bdfdf
Michali K <michali@gmail.com>
2016-12-17 00:05:22 +0000

    Added form elements Ignoring generated js and map files

--- wwwroot/app/home/home.component.ts
+++ wwwroot/app/home/home.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ICountry } from './country';
 
 @Component({
     moduleId: module.id,
     templateUrl: 'home.component.html'
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit{ 
+
+    countries: ICountry[]
+
+    ngOnInit(): void {
+        this.countries = [];
+        this.countries.push({id:1, name:"Sweden"});
+        this.countries.push({id:2, name:"Republic of Ireland"});       
+    }
+}
