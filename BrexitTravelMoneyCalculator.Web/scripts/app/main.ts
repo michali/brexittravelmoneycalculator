c75773b93e9e149f3b7817c332399e0f0cf0a679
Michali K <michali@gmail.com>
2017-07-11 15:51:26 +0300

    Importing zone.js and reflect-metadata straight from main
    To work around a problem where reflect-metadata isn't found after having installed Webpack.

--- BrexitTravelMoneyCalculator.Web/scripts/app/main.ts
+++ BrexitTravelMoneyCalculator.Web/scripts/app/main.ts
@@ -1,4 +1,6 @@
 //import { enableProdMode } from '@angular/core';
+import 'zone.js';
+import 'reflect-metadata';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
